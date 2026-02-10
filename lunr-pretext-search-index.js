6583cb041f973b1d61b53b4c91da8632a790b3ca
lilyclements <lily@idems.international>
2026-02-10 15:55:48 +0000

    Latest build deployed.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,265 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "release-notes",
+  "level": "1",
+  "url": "release-notes.html",
+  "type": "Preface",
+  "number": "",
+  "title": "Release Notes",
+  "body": " Release Notes   "
+},
+{
+  "id": "preface",
+  "level": "1",
+  "url": "preface.html",
+  "type": "Preface",
+  "number": "",
+  "title": "Preface",
+  "body": " Preface   "
+},
+{
+  "id": "sec-TIPVS",
+  "level": "1",
+  "url": "sec-TIPVS.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "Terminology: Individuals\/Population\/Variables\/Samples",
+  "body": " Terminology: Individuals\/Population\/Variables\/Samples   "
+},
+{
+  "id": "sec-VRoDICV",
+  "level": "1",
+  "url": "sec-VRoDICV.html",
+  "type": "Section",
+  "number": "1.2",
+  "title": "Visual Representation of Data, I: Categorical Variables",
+  "body": " Visual Representation of Data, I: Categorical Variables    Bar Charts I: Frequency Charts     Bar Charts II: Relative Frequency Charts     Bar Charts III: Cautions     Pie Charts    "
+},
+{
+  "id": "sec-VRoDIIQV",
+  "level": "1",
+  "url": "sec-VRoDIIQV.html",
+  "type": "Section",
+  "number": "1.3",
+  "title": "Visual Representation of Data, II: Quantitative Variables",
+  "body": " Visual Representation of Data, II: Quantitative Variables    Stem-and-leaf Plots     [Frequency] Histograms     [Relative Frequency] Histograms     How to Talk About Histograms    "
+},
+{
+  "id": "sec-NDoDIMotC",
+  "level": "1",
+  "url": "sec-NDoDIMotC.html",
+  "type": "Section",
+  "number": "1.4",
+  "title": "Numerical Descriptions of Data, I: Measures of the Center",
+  "body": " Numerical Descriptions of Data, I: Measures of the Center    Mode     Mean     Median     Strengths and Weaknesses of These Measures of Central Tendency    "
+},
+{
+  "id": "sec-NDoDIMoS",
+  "level": "1",
+  "url": "sec-NDoDIMoS.html",
+  "type": "Section",
+  "number": "1.5",
+  "title": "Numerical Descriptions of Data, II: Measures of Spread",
+  "body": " Numerical Descriptions of Data, II: Measures of Spread    Range     Quartiles and the IQR     Variance and Standard Deviation     Strengths and Weaknesses of These Measures of Spread     A Formal Definition of Outliers -- the 1.5 IQR Rule     The Five-Number Summary and Boxplots    "
+},
+{
+  "id": "sec-one-variable-exercises",
+  "level": "1",
+  "url": "sec-one-variable-exercises.html",
+  "type": "Section",
+  "number": "1.6",
+  "title": "Exercises",
+  "body": " Exercises   "
+},
+{
+  "id": "sec-TERoID",
+  "level": "1",
+  "url": "sec-TERoID.html",
+  "type": "Section",
+  "number": "2.1",
+  "title": "Terminology: Explanatory\/Response or Independent\/Dependent",
+  "body": " Terminology: Explanatory\/Response or Independent\/Dependent   "
+},
+{
+  "id": "sec-scatterplots",
+  "level": "1",
+  "url": "sec-scatterplots.html",
+  "type": "Section",
+  "number": "2.2",
+  "title": "Scatterplots",
+  "body": " Scatterplots   "
+},
+{
+  "id": "sec-correlation",
+  "level": "1",
+  "url": "sec-correlation.html",
+  "type": "Section",
+  "number": "2.3",
+  "title": "Correlation",
+  "body": " Correlation   "
+},
+{
+  "id": "sec-bivariate-exercises",
+  "level": "1",
+  "url": "sec-bivariate-exercises.html",
+  "type": "Section",
+  "number": "2.4",
+  "title": "Exercises",
+  "body": " Exercises   "
+},
+{
+  "id": "sec-TLSRL",
+  "level": "1",
+  "url": "sec-TLSRL.html",
+  "type": "Section",
+  "number": "3.1",
+  "title": "The Least Squares Regression Line",
+  "body": " The Least Squares Regression Line   "
+},
+{
+  "id": "sec-AaIoLSRLs",
+  "level": "1",
+  "url": "sec-AaIoLSRLs.html",
+  "type": "Section",
+  "number": "3.2",
+  "title": "Applications and Interpretations of LSRLs",
+  "body": " Applications and Interpretations of LSRLs   "
+},
+{
+  "id": "sec-Cs",
+  "level": "1",
+  "url": "sec-Cs.html",
+  "type": "Section",
+  "number": "3.3",
+  "title": "Cautions",
+  "body": " Cautions    Sensitivity to Outliers     Causation     Extrapolation     Simpson's Paradox    "
+},
+{
+  "id": "sec-linear-regression-exercises",
+  "level": "1",
+  "url": "sec-linear-regression-exercises.html",
+  "type": "Section",
+  "number": "3.4",
+  "title": "Exercises",
+  "body": " Exercises   "
+},
+{
+  "id": "sec-defsforprob",
+  "level": "1",
+  "url": "sec-defsforprob.html",
+  "type": "Section",
+  "number": "4.1",
+  "title": "Definitions for Probability",
+  "body": " Definitions for Probability    Sample Spaces, Set Operations, and Probability Models     Venn Diagrams     Finite Probability Models    "
+},
+{
+  "id": "sec-condprob",
+  "level": "1",
+  "url": "sec-condprob.html",
+  "type": "Section",
+  "number": "4.2",
+  "title": "Conditional Probability",
+  "body": " Conditional Probability   "
+},
+{
+  "id": "sec-RVs",
+  "level": "1",
+  "url": "sec-RVs.html",
+  "type": "Section",
+  "number": "4.3",
+  "title": "Random Variables",
+  "body": " Random Variables    Definition and First Examples     Distributions for Discrete RVs     Expectation for Discrete RVs     Density Functions for Continuous RVs     The Normal Distribution    "
+},
+{
+  "id": "sec-probability-exercises",
+  "level": "1",
+  "url": "sec-probability-exercises.html",
+  "type": "Section",
+  "number": "4.4",
+  "title": "Exercises",
+  "body": " Exercises   "
+},
+{
+  "id": "sec-SoaSPC",
+  "level": "1",
+  "url": "sec-SoaSPC.html",
+  "type": "Section",
+  "number": "5.1",
+  "title": "Studies of a Population Parameter",
+  "body": " Studies of a Population Parameter   "
+},
+{
+  "id": "sec-SoC",
+  "level": "1",
+  "url": "sec-SoC.html",
+  "type": "Section",
+  "number": "5.2",
+  "title": "Studies of Causality",
+  "body": " Studies of Causality    Control Groups     Human-Subject Experiments: The Placebo Effect     Blinding     Combining it all: RCTs     Confounded Lurking Variables    "
+},
+{
+  "id": "sec-EE",
+  "level": "1",
+  "url": "sec-EE.html",
+  "type": "Section",
+  "number": "5.3",
+  "title": "Experimental Ethics",
+  "body": " Experimental Ethics    \"Do No Harm\"     Informed Consent     Confidentiality     External Oversight [IRB]    "
+},
+{
+  "id": "sec-bringing-home-exercises",
+  "level": "1",
+  "url": "sec-bringing-home-exercises.html",
+  "type": "Section",
+  "number": "5.4",
+  "title": "Exercises",
+  "body": " Exercises   "
+},
+{
+  "id": "sec-CLT",
+  "level": "1",
+  "url": "sec-CLT.html",
+  "type": "Section",
+  "number": "6.1",
+  "title": "The Central Limit Theorem",
+  "body": " The Central Limit Theorem   "
+},
+{
+  "id": "sec-BCIs",
+  "level": "1",
+  "url": "sec-BCIs.html",
+  "type": "Section",
+  "number": "6.2",
+  "title": "Basic Confidence Intervals",
+  "body": " Basic Confidence Intervals    Cautions    "
+},
+{
+  "id": "sec-BHT",
+  "level": "1",
+  "url": "sec-BHT.html",
+  "type": "Section",
+  "number": "6.3",
+  "title": "Basic Hypothesis Testing",
+  "body": " Basic Hypothesis Testing    The Formal Steps of Hypothesis Testing     How Small is Small Enough, for p-values?     Calculations for Hypothesis Testing of Population Means     Cautions    "
+},
+{
+  "id": "sec-basic-inferences-exercises",
+  "level": "1",
+  "url": "sec-basic-inferences-exercises.html",
+  "type": "Section",
+  "number": "6.4",
+  "title": "Exercises",
+  "body": " Exercises   "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
